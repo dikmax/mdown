8b849ad1973ab0364f35c123c5ca27bc3d05a505
Maxim Dikun <dikmax@gmail.com>
2014-11-29 20:50:20 +0300

    String builder for HtmlWriter.

--- lib/src/html_writer.dart
+++ lib/src/html_writer.dart
@@ -2,7 +2,246 @@ library md_proc.html_writer;
 
 import 'definitions.dart';
 
-class HtmlWriter {
+class _HtmlBuilder extends StringBuffer {
+
+  _HtmlBuilder() : super();
+
+
+  void writeDocument(Document document) {
+    writeBlocks(document.contents);
+    write("\n");
+  }
+
+
+  // Blocks
+
+  void writeBlocks(Iterable<Block> blocks, {bool tight: false}) {
+    Iterator<Block> it = blocks.iterator;
+
+    bool first = true;
+    while (it.moveNext()) {
+      if (first) {
+        first = false;
+      } else {
+        write("\n");
+      }
+      Block block = it.current;
+
+      if (block is Para) {
+        if (tight) {
+          writeInlines(block.contents);
+        } else {
+          writePara(block);
+        }
+      } else if (block is Header) {
+        writeHeader(block);
+      } else if (block is HorizontalRule) {
+        write('<hr/>');
+      } else if (block is CodeBlock) {
+        writeCodeBlock(block);
+      } else if (block is Blockquote) {
+        writeBlockquote(block);
+      } else if (block is RawBlock) {
+        write(block.contents);
+      } else if (block is UnorderedList) {
+        writeUnorderedList(block);
+      } else if (block is OrderedList) {
+        writeOrderedList(block);
+      } else {
+        throw new UnimplementedError(block.toString());
+      }
+    }
+  }
+
+
+  void writeBlockquote(Blockquote blockquote) {
+    write("<blockquote>\n");
+    writeBlocks(blockquote.contents);
+    write("\n</blockquote>");
+  }
+
+
+  void writeHeader(Header header) {
+    write("<h");
+    write(header.level);
+    write(">");
+    writeInlines(header.contents);
+    write("</h");
+    write(header.level);
+    write(">");
+  }
+
+
+  void writeCodeBlock(CodeBlock codeBlock) {
+    write("<pre><code");
+    writeAttributes(codeBlock.attributes);
+    write(">");
+    write(htmlEscape(codeBlock.contents));
+    write("</code></pre>");
+  }
+
+  void writeListItems(ListBlock list) {
+    for (ListItem item in list.items) {
+      write("<li>");
+      writeBlocks(item.contents, tight: list.tight);
+      write("</li>\n");
+    }
+  }
+
+  void writeUnorderedList(UnorderedList list) {
+    write("<ul>\n");
+    writeListItems(list);
+    write("</ul>");
+  }
+
+
+  void writeOrderedList(OrderedList list) {
+    write("<ol");
+    if (list.startIndex != 1) {
+      write(' start="');
+      write(list.startIndex);
+      write('"');
+    }
+    write(">\n");
+    writeListItems(list);
+    write("</ol>");
+  }
+
+  void writePara(Para para) {
+    write("<p>");
+    writeInlines(para.contents);
+    write("</p>");
+  }
+
+
+  // Attributes
+
+  void writeAttributes(Attr attr) {
+    if (attr is EmptyAttr) {
+      return;
+    } else if (attr is InfoString) {
+      if (attr.language == "") {
+        return;
+      }
+      write(' class="language-');
+      write(attr.language);
+      write('"');
+    } else {
+      throw new UnimplementedError(attr.toString());
+    }
+  }
+
+
+  // Inlines
+
+  void writeInlines(Iterable<Inline> inlines, {bool stripped: false}) {
+    for (Inline inline in inlines) {
+      if (inline is Str) {
+        write(htmlEscape(inline.contents));
+      } else if (inline is Space) {
+        write(' ');
+      } else if (inline is NonBreakableSpace) {
+        write('\u{a0}');
+      } else if (inline is LineBreak) {
+        if (stripped) {
+          write(' ');
+        } else {
+          write('<br/>\n');
+        }
+      } else if (inline is Emph) {
+        writeEmph(inline, stripped: stripped);
+      } else if (inline is Strong) {
+        writeStrong(inline, stripped: stripped);
+      } else if (inline is Link) {
+        writeLink(inline, stripped: stripped);
+      } else if (inline is Image) {
+        writeImage(inline, stripped: stripped);
+      } else if (inline is Code) {
+        writeCodeInline(inline, stripped: stripped);
+      } else if (inline is RawInline) {
+        write(inline.contents);
+      } else {
+        throw new UnimplementedError(inline.toString());
+      }
+    }
+  }
+
+
+  void writeCodeInline(Code code, {bool stripped: false}) {
+    if (!stripped) {
+      write('<code>');
+    }
+    write(htmlEscape(code.contents));
+    if (!stripped) {
+      write('</code>');
+    }
+  }
+
+
+  void writeEmph(Emph emph, {bool stripped: false}) {
+    if (!stripped) {
+      write('<em>');
+    }
+    writeInlines(emph.contents, stripped: stripped);
+    if (!stripped) {
+      write('</em>');
+    }
+  }
+
+
+  void writeStrong(Strong strong, {bool stripped: false}) {
+    if (!stripped) {
+      write('<strong>');
+    }
+    writeInlines(strong.contents, stripped: stripped);
+    if (!stripped) {
+      write('</strong>');
+    }
+  }
+
+
+  void writeLink(Link link, {bool stripped: false}) {
+    if (!stripped) {
+      write('<a href="');
+      write(urlEncode(link.target.link));
+      write('"');
+      if (link.target.title != null) {
+        write(' title="');
+        write(htmlEscape(link.target.title));
+        write('"');
+      }
+      write('>');
+    }
+    writeInlines(link.label, stripped: stripped);
+    if (!stripped) {
+      write('</a>');
+    }
+  }
+
+
+  void writeImage(Image image, {bool stripped: false}) {
+    if (!stripped) {
+      write('<img src="');
+      write(urlEncode(image.target.link));
+      write('" alt="');
+      _HtmlBuilder builder = new _HtmlBuilder();
+      builder.writeInlines(image.label, stripped: true);
+      write(htmlEscape(builder.toString()));
+      write('"');
+      if (image.target.title != null) {
+        write(' title="');
+        write(htmlEscape(image.target.title));
+        write('"');
+      }
+      write(" />");
+    } else {
+      writeInlines(image.label, stripped: true);
+    }
+  }
+
+
+  // Escaping
+
   RegExp _escapedChars = new RegExp(r'[<>&"]');
   Map<String, String> _escape = <String, String>{
       "<": "&lt;",
@@ -10,183 +249,29 @@ class HtmlWriter {
       '"': "&quot;",
       "&": "&amp;"
   };
+
   String htmlEscape(String str) => str.replaceAllMapped(_escapedChars, (Match match) => _escape[match.group(0)]);
 
-  String write(Document document) => writeBlocks(document.contents);
-
-  String writeAttributes(Attr attr) {
-    if (attr is EmptyAttr) {
-      return '';
-    } else if (attr is InfoString) {
-      if (attr.language == "") {
-        return '';
-      }
-      return ' class="language-${attr.language}"';
-    }
-    throw new UnimplementedError(attr.toString());
-  }
-
-// Blocks
-  String writeBlocks(Iterable<Block> blocks) => writeBlocks_(blocks, false).map((item) => item + "\n").join();
-
-  String writeBlocksTight(Iterable<Block> blocks) => writeBlocks_(blocks, true).join('\n');
-
-  Iterable<String> writeBlocks_(Iterable<Block> blocks, bool tight) => blocks.map((Block block) {
-    if (block is Para) {
-      return tight ? writeParaTight(block) : writePara(block);
-    } else if (block is Header) {
-      return writeHeader(block);
-    } else if (block is HorizontalRule) {
-      return '<hr/>';
-    } else if (block is CodeBlock) {
-      return writeCodeBlock(block);
-    } else if (block is Blockquote) {
-      return writeBlockquote(block);
-    } else if (block is RawBlock) {
-      return block.contents;
-    } else if (block is UnorderedList) {
-      return writeUnorderedList(block);
-    } else if (block is OrderedList) {
-      return writeOrderedList(block);
-    }
-    throw new UnimplementedError(block.toString());
-  });
-
-  String writeBlockquote(Blockquote blockquote) => "<blockquote>\n${writeBlocks(blockquote.contents)}</blockquote>";
-
-  String writeHeader(Header header) => "<h${header.level}>${writeInlines(header.contents)}</h${header.level}>";
-
-  String writeCodeBlock(CodeBlock codeBlock) => "<pre><code${writeAttributes(codeBlock.attributes)}>" +
-  "${htmlEscape(codeBlock.contents)}</code></pre>";
-
-  String writeListItems(ListBlock list) {
-    if (list.tight) {
-      return list.items.map((ListItem item) => "<li>${writeBlocksTight(item.contents)}</li>\n").join();
-    } else {
-      return list.items.map((ListItem item) {
-        String blocks = writeBlocks(item.contents);
-        if (blocks.endsWith("\n")) {
-          blocks = blocks.substring(0, blocks.length - 1);
-        }
-        return "<li>${blocks}</li>\n";
-      }).join();
-    }
-  }
-  String writeUnorderedList(UnorderedList list) => "<ul>\n${writeListItems(list)}</ul>";
-  String writeOrderedList(OrderedList list) => "<ol${list.startIndex != 1 ? ' start="${list.startIndex}"' : ''}>\n${writeListItems(list)}</ol>";
-
-  String writePara(Para para) => "<p>${writeInlines(para.contents)}</p>";
-
-  String writeParaTight(Para para) => writeInlines(para.contents);
-
-  // Inlines
-  String writeInlines(Iterable<Inline> inlines) {
-    return inlines.map((Inline inline) {
-      if (inline is Str) {
-        return htmlEscape(inline.contents);
-      } else if (inline is Space) {
-        return ' ';
-      } else if (inline is NonBreakableSpace) {
-        return '\u{a0}';
-      } else if (inline is LineBreak) {
-        return '<br/>\n';
-      } else if (inline is Emph) {
-        return writeEmph(inline);
-      } else if (inline is Strong) {
-        return writeStrong(inline);
-      } else if (inline is Link) {
-        return writeLink(inline);
-      } else if (inline is Image) {
-        return writeImage(inline);
-      } else if (inline is Code) {
-        return writeCodeInline(inline);
-      } else if (inline is RawInline) {
-        return inline.contents;
-      }
-
-      throw new UnimplementedError(inline.toString());
-    }).join();
-  }
-
-  String writeInlinesStripped(Iterable<Inline> inlines) {
-    return inlines.map((Inline inline) {
-      if (inline is Str) {
-        return htmlEscape(inline.contents);
-      } else if (inline is Space) {
-        return ' ';
-      } else if (inline is NonBreakableSpace) {
-        return '\u{a0}';
-      } else if (inline is LineBreak) {
-        return ' ';
-      } else if (inline is Emph) {
-        return writeEmphStripped(inline);
-      } else if (inline is Strong) {
-        return writeStrongStripped(inline);
-      } else if (inline is Link) {
-        return writeLinkStripped(inline);
-      } else if (inline is Image) {
-        return writeImageStripped(inline);
-      } else if (inline is Code) {
-        return writeCodeInlineStripped(inline);
-      } else if (inline is RawInline) {
-        return inline.contents;
-      }
-
-      throw new UnimplementedError(inline.toString());
-    }).join();
-  }
-
-  String writeCodeInline(Code code) {
-    return '<code>${htmlEscape(code.contents)}</code>';
-  }
-
-  String writeCodeInlineStripped(Code code) {
-    return htmlEscape(code.contents);
-  }
-
-  String writeEmph(Emph emph) {
-    return '<em>${writeInlines(emph.contents)}</em>';
-  }
-
-  String writeEmphStripped(Emph emph) {
-    return writeInlinesStripped(emph.contents);
-  }
-
-  String writeStrong(Strong strong) {
-    return '<strong>${writeInlines(strong.contents)}</strong>';
-  }
-
-  String writeStrongStripped(Strong strong) {
-    return writeInlinesStripped(strong.contents);
-  }
 
   RegExp _urlEncode = new RegExp(r'%[0-9a-fA-F]{2}');
   RegExp _htmlEntity = new RegExp(r'&(?:#x[a-f0-9]{1,8}|#[0-9]{1,8}|[a-z][a-z0-9]{1,31});', caseSensitive: false);
+
   String urlEncode(String url) {
     url = url.splitMapJoin(_urlEncode, onMatch: (Match m) => m.group(0), onNonMatch: (String s) => Uri.encodeFull(s));
     url = url.splitMapJoin(_htmlEntity, onMatch: (Match m) => m.group(0), onNonMatch: (String s) => htmlEscape(s));
     return url;
   }
+}
 
-  String writeLink(Link link) {
-    return '<a href="${urlEncode(link.target.link)}"' +
-    (link.target.title != null ? ' title="${htmlEscape(link.target.title)}"' : '') +
-    ">${writeInlines(link.label)}</a>";
+
+class HtmlWriter {
+
+  String write(Document document) {
+    _HtmlBuilder builder = new _HtmlBuilder();
+    builder.writeDocument(document);
+    return builder.toString();
   }
 
-  String writeLinkStripped(Link link) {
-    return writeInlinesStripped(link.label);
-  }
-
-  String writeImage(Image image) {
-    return '<img src="${urlEncode(image.target.link)}" alt="${htmlEscape(writeInlinesStripped(image.label))}"' +
-    (image.target.title != null ? ' title="${htmlEscape(image.target.title)}"' : '') +
-    " />";
-  }
-
-  String writeImageStripped(Image image) {
-    return writeInlinesStripped(image.label);
-  }
 
   static HtmlWriter DEFAULT = new HtmlWriter();
 }
