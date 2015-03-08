cf70f6f6325bb5bd46a47c8aca9874c7d8fcf380
Maxim Dikun <dikmax@gmail.com>
2015-03-08 21:13:49 +0300

    Travis build with coverall support.

---
+++ tool/travis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+# Verify that the libraries are error free.
+dartanalyzer --fatal-warnings \
+  lib/md_proc.dart \
+  lib/src/definitions.dart \
+  lib/src/entities.dart \
+  lib/src/html_writer.dart \
+  lib/src/markdown_parser.dart \
+  lib/src/markdown_writer.dart \
+  lib/src/options.dart \
+  test/parser_test.dart
+
+# Run the tests.
+dart --checked test/parser_test.dart
+
+# If the COVERALLS_TOKEN token is set on travis
+# Install dart_coveralls
+# Rerun tests with coverage and send to coveralls
+if [ "$COVERALLS_TOKEN" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $COVERALLS_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/parser_test.dart
+fi
