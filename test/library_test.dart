ee0257d929a648fa3d0d95d98908cb47f370c5af
Maxim Dikun <dikmax@gmail.com>
2015-08-11 18:05:01 +0300

    Formatting for library_test.

--- test/library_test.dart
+++ test/library_test.dart
@@ -16,15 +16,21 @@ void main() {
   //fileTest("CommonMark", specification, mdToHtmlTest(Options.STRICT, (t, num) => t == TestType.HTML && (num == 129)));
   //fileTest("CommonMark", specification, mdToHtmlTest(Options.STRICT, (t, num) => t == TestType.HTML && (num >= 103 && num < 144)));
   //fileTest("CommonMark", specification, mdToHtmlTest(Options.STRICT, (t, num) => t == TestType.HTML));
-  tests("CommonMark", specification, mdToHtmlTest(Options.strict));
+  tests("CommonMark [strict]", specification, mdToHtmlTest(Options.strict));
+
   // Additional tests
   //fileTest("Additional", additionalMarkdownToHtml, mdToHtmlTest(Options.STRICT, (t, num) => t == TestType.HTML && num == 19));
-  tests("Additional", additionalMarkdownToHtml, mdToHtmlTest(Options.strict));
-  // Additional tests
-  tests("SmartPunct", smartPunctuation, mdToHtmlTest(Options.defaults));
-  tests("Strikeout", strikeout, mdToHtmlTest(Options.defaults));
+  tests("Additional [strict]", additionalMarkdownToHtml, mdToHtmlTest(Options.strict));
+
+  // SmartPunct
+  tests("SmartPunct [commonmark]", smartPunctuation, mdToHtmlTest(Options.commonmark));
+
+  // Strikeout
+  tests("Strikeout [defaults]", strikeout, mdToHtmlTest(Options.defaults));
+
   // Markdown to markdown tests
-  tests("md2md", markdownToMarkdown, mdToMdTest(Options.strict));
+  tests("md2md [strict]", markdownToMarkdown, mdToMdTest(Options.strict));
+
   // Custom resolver
   referenceResolverTests();
 }
