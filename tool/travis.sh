d5bb95fb72d9c183e6013e3f196805e9be75d17f
Maxim Dikun <dikmax@gmail.com>
2016-11-20 13:26:30 +0100

    Fix coverage collection.

--- tool/travis.sh
+++ tool/travis.sh
@@ -14,8 +14,8 @@ dartanalyzer --strong --fatal-warnings \
 echo "Running tests"
 pub run test --reporter json -p "vm" | dart tool/reporter.dart
 
-pub run coverage:collect_coverage --port=8111 -o coverage.json --resume-isolates &
-dart --enable-vm-service=8111 --checked test/library_test.dart
+dart --enable-vm-service=8111 --pause-isolates-on-exit --checked test/library_test.dart &
+pub run coverage:collect_coverage --port=8111 -o coverage.json --wait-paused --resume-isolates
 pub run coverage:format_coverage --report-on=lib --packages=.packages --in=coverage.json --out=lcov.info --lcov
 
 echo "Done"
