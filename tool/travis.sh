f703fced87d871a0b7efeb0ace1b3647e646eae8
Maxim Dikun <dikmax@gmail.com>
2016-11-20 12:43:56 +0100

    One more attempt to fix coverage.

--- tool/travis.sh
+++ tool/travis.sh
@@ -4,20 +4,18 @@
 set -e
 
 # Verify that the libraries are error free.
-#dartanalyzer --strong --fatal-warnings \
-#  lib/md_proc.dart \
-#  test/library_test.dart \
-#  tool/build.dart \
-#  tool/reporter.dart
+dartanalyzer --strong --fatal-warnings \
+  lib/md_proc.dart \
+  test/library_test.dart \
+  tool/build.dart \
+  tool/reporter.dart
 
 # Run the tests.
 echo "Running tests"
 pub run test --reporter json -p "vm" | dart tool/reporter.dart
 
-dart --observe=8111 --checked test/library_test.dart & # start in background
-sleep 1
-pub run coverage:collect_coverage --port=8111 -o coverage.json --resume-isolates
-wait %1
+pub run coverage:collect_coverage --port=8111 -o coverage.json --resume-isolates &
+dart --enable-vm-service=8111 --checked test/library_test.dart
 pub run coverage:format_coverage --report-on lib --in coverage.json --out lcov.info --lcov
 
 echo "Done"
