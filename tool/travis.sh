da80d6c7f70c707e6ab99a22aa857e7aea035088
Maxim Dikun <dikmax@gmail.com>
2016-11-20 13:01:45 +0100

    Fix lcov generation.

--- tool/travis.sh
+++ tool/travis.sh
@@ -16,6 +16,6 @@ pub run test --reporter json -p "vm" | dart tool/reporter.dart
 
 pub run coverage:collect_coverage --port=8111 -o coverage.json --resume-isolates &
 dart --enable-vm-service=8111 --checked test/library_test.dart
-pub run coverage:format_coverage --report-on lib --in coverage.json --out lcov.info --lcov
+pub run coverage:format_coverage --report-on=lib --packages=.packages --in=coverage.json --out=lcov.info --lcov
 
 echo "Done"
