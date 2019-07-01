4ec9915ed326d15a8980af9807de22a9c6e5d68f
Maxim Dikun <dikmax@gmail.com>
2019-07-01 22:10:23 +0200

    Enable coverage.

--- tool/travis.sh
+++ tool/travis.sh
@@ -14,8 +14,8 @@ dartanalyzer --fatal-warnings \
 echo "Running tests"
 pub run test --reporter json -p "vm" | dart tool/reporter.dart
 
-# dart --enable-vm-service=8111 --pause-isolates-on-exit test/library_test.dart &
-# pub run coverage:collect_coverage --port=8111 -o coverage.json --wait-paused --resume-isolates
-# pub run coverage:format_coverage --report-on=lib --packages=.packages --in=coverage.json --out=lcov.info --lcov
+dart --enable-vm-service=8111 --disable-service-auth-codes --pause-isolates-on-exit test/library_test.dart &
+pub run coverage:collect_coverage --port=8111 -o coverage.json --wait-paused --resume-isolates
+pub run coverage:format_coverage --report-on=lib --packages=.packages --in=coverage.json --out=lcov.info --lcov
 
 echo "Done"
